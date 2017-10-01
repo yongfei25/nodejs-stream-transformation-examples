1fc144da8d02b2f5d6750d9d0f319d6eac4c6585
Yong Fei <yongfei@iflix.com>
2017-10-01 16:33:01 +0800

    First example.

---
+++ examples/1-copy-unzip.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const zlib = require('zlib')
+const path = require('path')
+const dataLib = require('./data-lib')
+
+// Declare the paths
+const sourceFile = dataLib.getMockDataPath()
+const targetPath = path.join(dataLib.getOutputDir(), '1-copy-unzip-result')
+
+// Create a Readable stream
+const read = fs.createReadStream(sourceFile)
+
+// zlib.createGunzip() returns a Transform stream that gunzip the data it receives
+const transform = zlib.createGunzip()
+
+// Stream is a type of EventEmitter.
+// Attach an event listener to print the data it returns.
+transform.on('data', (data) => console.log(data.toString('utf-8')))
+
+// Create a Writable Stream
+const write = fs.createWriteStream(targetPath)
+write.on('error', () => write.end())
+write.on('finish', () => console.log('Done writing to file.'))
+
+// Ends the Writable stream on error
+read.on('error', () => write.end())
+
+// Start pushing the data
+read.pipe(transform).pipe(write)
